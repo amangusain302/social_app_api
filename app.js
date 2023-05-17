a3e8324da1e2f5ea97772b864a09313259065a59
amangusain302 <amangusain302@gmail.com>
2023-05-18 01:57:06 +0530

    initialise project , error handling , app js

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const errorMiddleware = require("./src/middlewares/Error");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('common'));
+app.use(helmet());
+
+
+// app.use('/api/v1', )
+
+app.get('/', (req, res) => {
+    res.send("<h1>welcome to New Gen Social App</h1>");
+})
+
+
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, (err, data) => {
+    console.log(`Server is running on ${port} port`);
+})
---
+++ src/middlewares/Error.js
@@ -0,0 +1,12 @@
+const errorMiddleware = (err, req, res, next) => {
+
+    err.statusCode = err.statusCode || 500;
+    err.message = errr.message || "Internal Server Error";
+
+    res.status(err.statusCode).json({
+        success: false,
+        error: err.message
+    })
+}
+
+module.exports = errorMiddleware;
